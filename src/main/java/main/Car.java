0afeea31ae1c258ead5b4922266743b946fcf698
JBOhge <ohgejb@gmail.com>
2021-05-12 12:09:07 -0500

    update

--- src/main/java/main/Car.java
+++ src/main/java/main/Car.java
@@ -1,4 +1,29 @@
 package main;
 
 public class Car {
+    private String engine;
+    private int miles;
+
+
+    Car(String engine, int mile){
+        this.engine=engine;
+        this.miles=miles;
+    }
+
+
+    public String getEngine() {
+        return engine;
+    }
+
+    public void setEngine(String engine) {
+        this.engine = engine;
+    }
+
+    public int getMiles() {
+        return miles;
+    }
+
+    public void setMiles(int miles) {
+        this.miles = miles;
+    }
 }
--- src/main/java/main/Driver.java
+++ src/main/java/main/Driver.java
@@ -1,8 +1,18 @@
 package main;
 
+import java.sql.SQLOutput;
+
 public class Driver {
 
     public static void main(String[] args) {
 
+        Car myCar = new Car("V6",50837);
+        Car anotherCar = new Car("V4", 120894);
+
+
+        Person mike = new Person("Mike", 25);
+        Person john = new Person("John", 28);
+
+        System.out.println("Matt is great");
     }
 }
